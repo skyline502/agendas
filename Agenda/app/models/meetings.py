367d7e4ceb928a96c16ff4b74c9535f9b0eb008b
Johnny San <johnny.san@gmail.com>
2022-06-30 14:02:51 -0700

    app is loading

--- Agenda/app/models/meetings.py
+++ Agenda/app/models/meetings.py
@@ -7,3 +7,9 @@ class Meeting(db.Model):
   __tablename__ = 'meetings'
 
   id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100), nullable=False)
+  presenter_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  start = db.Column(db.Datetime(timezone=True))
+  end = db.Column(db.Datetime(timezone=True))
+  created_at = db.Column(db.Datetime(timezone=True), server_default=func.now())
+  updated_at = db.Column(db.Datetime(timezone=True), server_default=func.now())
