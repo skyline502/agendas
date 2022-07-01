73b520ca9bc686c314cf35dd8c10b1c623205b64
Johnny San <johnny.san@gmail.com>
2022-07-01 11:49:42 -0700

    added buttons

--- Agenda/react-app/src/components/Home.js
+++ Agenda/react-app/src/components/Home.js
@@ -1,44 +1,53 @@
-import React, {useEffect} from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getAllMeetings } from '../store/meetings';
-import './Home.css';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllMeetings } from "../store/meetings";
+import "./Home.css";
 
 const Home = () => {
-  const user = useSelector(state => state.session.user);
-  const meetings = useSelector(state => state.meetings.meetings);
+  const user = useSelector((state) => state.session.user);
+  const meetings = useSelector((state) => state.meetings.meetings);
   const dispatch = useDispatch();
   let date = new Date();
-  let formattedDate = date.toDateString().split(' ').slice(1)
-  formattedDate[0] += '.';
-  formattedDate[1] += ',';
-  formattedDate.join(' ');
+  let formattedDate = date.toDateString().split(" ").slice(1);
+  formattedDate[0] += ".";
+  formattedDate[1] += ",";
+  formattedDate.join(" ");
 
-  console.log(meetings, 'meetings....')
+  console.log(meetings, "meetings....");
   useEffect(() => {
     dispatch(getAllMeetings());
-  },[dispatch])
+  }, [dispatch]);
 
   // const meetings = useSelector(state => state.session.meetings);
-  console.log(user, 'this is the user');
+  console.log(user, "this is the user");
   // console.log(meetings, 'these are the meetings')
   return (
-    <div className='home-container'>
+    <div className="home-container">
       <h1>{formattedDate}'s Agenda</h1>
       <h2>Meetings Today</h2>
-      {meetings?.map(meeting => (
-        <div key={meeting.id} className='meeting-container'>
+      <button>Add Meeting</button>
+      {meetings?.map((meeting) => (
+        <div key={meeting.id} className="meeting-container">
           <div>Meeting: {meeting.title}</div>
           <div>
-            <ul>Topics:
-              {meeting.topics?.map(topic => (
+            <ul>
+              Topics:
+              {meeting.topics?.map((topic) => (
                 <div key={topic.id}>
-                  <div>{topic.title}:{topic.description}({topic.time_estimate}&nbsp;minutes)</div>
-                  {topic.comments?.map(comment => (
-                    <div key={comment.id}>{comment.user_id.username}:{comment.comment}</div>
+                  <div>
+                    {topic.title}:{topic.description}({topic.time_estimate}
+                    &nbsp;minutes)
+                  </div>
+                  {topic.comments?.map((comment) => (
+                    <div key={comment.id}>
+                      {comment.user_id.username}:{comment.comment}
+                    </div>
                   ))}
+                  <button>comment</button>
                 </div>
               ))}
             </ul>
+            <button>Add Topic</button>
           </div>
           <div>Start: {meeting.start}</div>
           <div>End: {meeting.end}</div>
@@ -46,7 +55,7 @@ const Home = () => {
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
 export default Home;
