e84301474e6e62ebcabc3acaa0d996813f6a9f50
Johnny <johnny.san@gmail.com>
2022-07-02 17:24:42 -0700

    added navbar styling

--- Agenda/react-app/src/components/NavBar.js
+++ Agenda/react-app/src/components/NavBar.js
@@ -3,33 +3,34 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 import { useSelector } from 'react-redux';
+import './NavBar.css';
 
 const NavBar = () => {
   const user = useSelector(state => state.session.user);
 
   if (user) {
     return (
-      <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Employees
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
+      <nav className='nav-bar'>
+        <ul>
+          <li>
+            <NavLink to='/' exact={true} activeClassName='active'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/users' exact={true} activeClassName='active'>
+              Employees
+            </NavLink>
+          </li>
+          <li>
+            <LogoutButton />
+          </li>
+        </ul>
+      </nav>
     )
   } else {
     return (
-      <nav>
+      <nav className='nav-bar'>
         <ul>
           <li>
             <NavLink to='/' exact={true} activeClassName='active'>
